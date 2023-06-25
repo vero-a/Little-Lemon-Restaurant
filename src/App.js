034336a20d9ecd0cb245d8b82bc83bccf7d9827a
Veroneca Abou faissal <veroneca@Veronecas-MacBook-Pro.local>
2023-06-25 17:26:46 -0400

    Semantic tags to App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <homepage>
+    <header className="App">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </header>;
+    <main></main>;
+    <footer></footer>;
+    </homepage>
   );
 }
 
 export default App;
+//need to add <a> hyperlinks for each nav link. Also change semantics to child component ex. <Header/>
