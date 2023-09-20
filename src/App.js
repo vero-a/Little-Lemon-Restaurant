c12d3f04815c3313654759aac6c10aa7ec5ced56
Veroneca Abou faissal <veroneca@Veronecas-MacBook-Pro.local>
2023-09-20 00:50:37 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { HashRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/'>
         <Navbar/>
         <Routing/>
         <Footer/>
