71421a50734c55b442c3595f90634e854523c13f
Veroneca Abou faissal <veroneca@Veronecas-MacBook-Pro.local>
2023-09-21 14:53:27 -0400

    gh-pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Routing from './Components/Routing';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename='/Little-Lemon-Restaurant'>
+    <Router>
         <Navbar/>
         <Routing/>
         <Footer/>
