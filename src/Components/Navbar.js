b80e10c8aa597caa655c6bc5203797cdf350778a
Veroneca Abou faissal <veroneca@Veronecas-MacBook-Pro.local>
2023-09-22 17:14:20 -0400

    Adjusted for mobile

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 import Logo from '../assets/Logo.svg';
 import Nav from './Nav';
 import HamburgerMenu from '../assets/hamburgermenu.svg';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import {AiOutlineClose} from 'react-icons/ai';
+
 
 function Navbar() {
     const [navbarOpen, setNavbarOpen] = useState(false);
@@ -14,7 +15,7 @@ function Navbar() {
         <nav className='nav-icon'>
             <img src={Logo} alt='Logo of Little Lemon Restaurant' className='nav-logo'/>
         <button className='hamburger-icon' onClick={toggleNavbar}>
-            <img src={navbarOpen ? faXmark : HamburgerMenu} alt='Icon' />
+            <img src={navbarOpen ? AiOutlineClose : HamburgerMenu} alt='Icon' />
         </button>
         </nav>
         <Nav device="desktop"/>
